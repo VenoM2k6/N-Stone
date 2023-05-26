3c01828cc2d6bdde99d72e204f3b792ccb65848a
VenoM2k6 <v.witschelinski@web.de>
2023-05-26 23:54:18 +0200

    Habe das automatische Anpassen gefixxt
    Es kann sein dass die Font.Size der Edits sich in ihrer Höhe schlecht anpassen(das müsste man noch test).

--- Prototype/backup/unit2.pas
+++ Prototype/backup/unit2.pas
@@ -78,6 +78,37 @@ begin
 
     hpplayer2.font.size := Round(hpplayer1.font.size*Widthdifference);
     manaplayer2.font.size := Round(hpplayer1.font.size*Widthdifference);
+
+
+    { background.left:=300*2;
+     background.top:=300;
+     background.width:=self.width -4*  300;
+     background.height:=self.height -2*  300;
+
+     player1Show.width:=background.width;
+     player1Show.height:=300-150;
+     player1Show.top:=150;
+     player1Show.left:=background.left;
+
+     player2Show.width:=background.width;
+     player2Show.height:=300-150;
+     player2Show.top:=300+background.height;
+     player2Show.left:=background.left;
+
+     hpplayer2.top:=player1Show.top+40;
+     manaplayer2.top:=hpplayer2.top+40;
+     hpplayer2.left:=player1Show.left + 1000;
+     manaplayer2.left:=player1Show.left + 1000;
+
+
+     hpplayer1.top:=player2Show.top+40;
+     manaplayer1.top:=hpplayer1.top+40;
+     hpplayer1.left:=player1Show.left + 1000;
+     manaplayer1.left:=player1Show.left + 1000;
+
+     exitImage.top:=player1Show.top;
+     exitImage.left:=40;
+     exitImage.height:=player1Show.height; }
 end;
 
 procedure Tingame.exitImageClick(Sender: TObject);
